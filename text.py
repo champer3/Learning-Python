cb655e20db0ce64324b36b0c32785836b516acdf
champer3 <okunolas21@gmail.com>
2022-07-28 20:43:38 +0100

    Learning python

--- text.py
+++ text.py
@@ -1,7 +1,25 @@
-def greet_user(name):
-    print(f"Hi there! {name}")
-    print("Welcome Aboard!")
+def groupAnagrams(strs):
+    fin_arr = []
+    new_arr = []
+    for str in strs:
+        geg = sorted(str)
+        j_arr = "".join(geg)
+        new_arr.append(j_arr)
+    print(new_arr)
+    for new in new_arr:
+        if [new] in fin_arr:
+            fin_arr[fin_arr.index([new])].append(new)
+        else:
+            fin_arr.append([new])
+    print(fin_arr)
 
-print("Start")
-greet_user("stephen")
-print('end')
+
+
+groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+
+
+jump = [0]
+
+jump[jump.index([1,2,3])].append("bolu")
+print(jump[0])
+
