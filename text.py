cba5905c34ad98c632e3ccd22d525ae81e87163f
champer3 <okunolas21@gmail.com>
2022-08-01 07:24:33 +0100

    learning python

--- text.py
+++ text.py
@@ -1,25 +1,33 @@
-def groupAnagrams(strs):
-    fin_arr = []
-    new_arr = []
-    for str in strs:
-        geg = sorted(str)
-        j_arr = "".join(geg)
-        new_arr.append(j_arr)
-    print(new_arr)
-    for new in new_arr:
-        if [new] in fin_arr:
-            fin_arr[fin_arr.index([new])].append(new)
-        else:
-            fin_arr.append([new])
-    print(fin_arr)
+# Leetcode Coding Chalenge
+#Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+from collections import defaultdict
 
 
+class Solution:
+    def groupAnagrams(strs):
+        res = defaultdict(list) # A dictionary that doesn't return keyError
+        for str in strs:
+            count = [0] * 26 # 26 zeros in an array to represent alphabet a - z
+            for c in str:
+                count[ord(c) - ord("a")] += 1 # matches each given string
+            res[tuple(count)].append(str)
+        return res.values() # return only the values in the hashmap
 
-groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+# Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])
 
 
-jump = [0]
+jump = [0] * 26
 
-jump[jump.index([1,2,3])].append("bolu")
-print(jump[0])
+jump[0] += 1
+jump[12] += 1
+jump[18] += 1
 
+print(jump)
+res = {
+    1: "love",
+    2: "koboko",
+    "dh": "pol"
+}
+res[1]
+# print(res.values())
