f90d0ae9f2056e375c0f06fb939f9c7f19b95381
champer3 <okunolas21@gmail.com>
2022-08-07 13:57:59 +0100

    Permutation stuff in python

--- list-arr-interview.py
+++ list-arr-interview.py
@@ -1,5 +1,6 @@
 # Some Array and List Interview Questions
 
+from xml.etree.ElementTree import TreeBuilder
 import numpy as np
 
 # Question 1 - Missing number in an array
@@ -57,4 +58,62 @@ def findMaxProduct(arr):
     return maxProduct
 
 
-print(findMaxProduct(myArray))
+# print(findMaxProduct(myArray))
+
+# Question 4 - Is Unique: Impliment an algorithm to see if a list has all unique characters
+
+
+def isUnique(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                return False
+    return True
+
+
+# print(isUnique(myArray))
+
+def isUnique1(arr):
+    newList = []
+    for num in arr:
+        if num in newList:
+            print(num)
+            return False
+        else:
+            newList.append(num)
+    print(newList)
+    return True
+
+
+# print(isUnique1(myArray))
+
+# Question 6 - Permutation
+
+str1 = "rail"
+str2 = "liar"
+
+myArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+myArr2 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+def isPermutation(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+    for num in arr1:
+        if num not in arr2:
+            return False
+    return True
+
+
+def isPermutation1(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+    arr1.sort()
+    arr2.sort()
+    if arr1 == arr2:
+        return True
+    else:
+        return False
+
+
+print(isPermutation1(myArr1, myArr2))
