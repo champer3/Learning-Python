928afab5b32b38f4df5445be6cbba53243326908
champer3 <okunolas21@gmail.com>
2022-08-18 01:43:35 +0100

    anagram shit

--- list-int.py
+++ list-int.py
@@ -2,6 +2,9 @@
 # and return false if every element is distinct.
 
 # Solution 1 - BRUTE FORCE
+from collections import Counter
+
+
 def containsDuplicate(nums):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
@@ -28,7 +31,7 @@ print(containsDuplicate1(nums1))
 
 # Question 2 - Given two strings s and t, return true if t is an anagram of s, and false otherwise
 
-# Solution 1 - BRUTE FORCE
+# Solution 1 - Array o(n)
 
 
 def isAnagram(s, t):
@@ -48,3 +51,46 @@ def isAnagram(s, t):
         else:
             return False
     return True
+
+# Solution 3 - Hashmap o(n)
+
+
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    countS, countT = {}, {}
+    for c in s:
+        countS[c] = 1 + countS.get(c, 0)
+    for c in t:
+        countT[c] = 1 + countT.get(c, 0)
+    for key in countS:
+        if countS[key] != countT.get(key, 0):
+            return False
+    return True
+
+    print(countS)
+
+# Solution 4 - Python3 Builtin Hasmap o(n)
+
+
+def isAnagram1(s, t):
+    if len(s) != len(t):
+        return False
+    return Counter(s) == Counter(t)
+
+
+s = "anagram"
+t = "nagaram"
+print(sorted(s))
+isAnagram1(s, t)
+
+
+# Solution 2 - SORT ALGORITHM o(nlogn)
+
+def isAnagram(s, t):
+    s = sorted(s)
+    t = sorted(t)
+    if t == s:
+        return True
+    else:
+        return False
